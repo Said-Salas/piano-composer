129300f2b6525ed717451d256b7ca3627f13fa75
Said Salas <saidsalastech@gmail.com>
2026-02-04 21:00:54 -0600

    piano 0.03

---
+++ app/components/Piano.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import { useEffect, useState } from 'react'
+import * as Tone from 'tone'
+
+export const Piano = () => {
+    const [synth, setSynth] = useState<Tone.PolySynth | null>(null)
+}
